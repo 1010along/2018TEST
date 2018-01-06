7b71169895ed7689a3892085a22955294270794d
linxinglong <linxinglong@126.com>
2018-01-06 15:59:43 +0800

    新建三级窗体

---
+++ 3级窗体/3级窗体/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3级窗体
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 land = new Form1();        //先弹出窗体1
+
+            if (land.ShowDialog() == DialogResult.OK)
+            {
+
+                Form2 land2 = new Form2();        //先弹出窗体1   
+                if (land2.ShowDialog() == DialogResult.OK)
+                {
+                    Application.Run(new Form3());
+                }
+             }
+
+            
+        }
+    }
+}
